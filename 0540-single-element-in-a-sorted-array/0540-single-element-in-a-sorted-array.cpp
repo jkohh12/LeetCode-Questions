82e672a031aae05871f1e9dc0f594b41b644ff6e
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-08-24 15:11:30 -0700

    Time: 12 ms (97.70%), Space: 22.4 MB (33.35%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -24,37 +24,62 @@ public:
         
         //O(logn) approach
         //first occurence is at even index, odd occurence is at odd index
-        int high = nums.size()-1;
-        int low = 0;
-        int mid;
+//         int high = nums.size()-1;
+//         int low = 0;
+//         int mid;
         
-        //Boundary Checks
-        if(high == 0) {
-            return nums[0];
-        }
-        else if(nums[0] != nums[1]) {
-            return nums[0];
-        }
-        else if(nums[high] != nums [high - 1]) {
-            return nums[high];
-        }
+//         //Boundary Checks
+//         if(high == 0) {
+//             return nums[0];
+//         }
+//         else if(nums[0] != nums[1]) {
+//             return nums[0];
+//         }
+//         else if(nums[high] != nums [high - 1]) {
+//             return nums[high];
+//         }
         
-        while (low <= high) {
-            mid = low + (high - low)/2;
-            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid-1]) { //check if mid is not equal to one before and one after
-                return nums[mid];
+//         while (low <= high) {
+//             mid = low + (high - low)/2;
+//             if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid-1]) { //check if mid is not equal to one before and one after
+//                 return nums[mid];
+//             }
+//             if(((mid % 2) == 0 && nums[mid] == nums[mid + 1]) || 
+//                ((mid % 2) == 1 && nums[mid] == nums[mid-1])) { //so if mid is even and the index after it equals it, then we check right side, if its odd and index before it equals it, then 
+//                 low = mid + 1;
+//             }
+//             else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return -1;
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while (start <= end) {
+            int mid = (start + end)/2;
+            if(start == end) {
+                return nums[start];
             }
-            if(((mid % 2) == 0 && nums[mid] == nums[mid + 1]) || 
-               ((mid % 2) == 1 && nums[mid] == nums[mid-1])) { //so if mid is even and the index after it equals it, then we check right side, if its odd and index before it equals it, then we also check right since its in order
-                low = mid + 1;
+            else if(mid % 2 != 0) { //odd
+                if(nums[mid - 1] == nums[mid]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
             }
             else {
-                high = mid - 1;
+                if(nums[mid] == nums[mid + 1]) {
+                    start = mid + 2;
+                }
+                else {
+                    end = mid;
+                }
             }
         }
         return -1;
-        
-
         
     }
 };
