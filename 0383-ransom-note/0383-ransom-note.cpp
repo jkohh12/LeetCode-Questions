a5c8fe791be1eef56a4ec929af86885cb9b59e7d
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-08-26 15:37:18 -0700

    Time: 13 ms (77.16%), Space: 8.9 MB (34.13%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,12 +3,12 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> dictionary;
         
-        for(auto c : magazine) {
+        for(auto c : magazine) { //loops through characters of magazine and build unordered map that counts occurence of each character
             dictionary[c]++;
         }
         
-        for(auto c : ransomNote) {
-            if(dictionary.find(c) == dictionary.end() || dictionary[c] <= 0) {
+        for(auto c : ransomNote) { //loops through characters in ransomNote 
+            if(/* dictionary.find(c) == dictionary.end() || */dictionary[c] <= 0) { //
                 return false;
             }
             dictionary[c]--;
