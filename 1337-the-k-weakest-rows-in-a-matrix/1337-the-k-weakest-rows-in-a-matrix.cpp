7116d7b4693aa35ab6aa3e58c1a86e8c39277865
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-08-26 17:39:03 -0700

    Time: 6 ms (95.52%), Space: 10.5 MB (29.33%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,16 +1,18 @@
 class Solution {
-private:
-    int bsearch(vector<int>& v){
-        int left = 0, right=v.size()-1, mid;
-        while(left<=right){
-            mid = left + (right - left)/2;
-            
-            if(v[mid]==1){
+private: 
+    int bSearch(vector<int>& v)  {
+        int left = 0,right=v.size()-1, mid;
+        //use bSearch to count # of 1's
+        //use the index of where last 1 is + 1
+        while(left <= right) { //basically if we find a 1 at mid index, that means we wanna set left to mid + 1, since that index is the number of 1's that exist in the vector
+            mid = (left+right)/2;
+            if(v[mid] == 1) {
                 left = mid + 1;
             }
-            else{
-                right = mid-1;
+            else {
+                right = mid - 1;
             }
+            
         }
         return left;
     }
@@ -19,17 +21,22 @@ public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         int n = mat.size();
         vector<pair<int,int>> vp;
-        int count = 0;
         
-        for(int i=0; i<n; ++i){
-            count = bsearch(mat[i]);
-            vp.push_back({count, i});
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            count = bSearch(mat[i]);
+            vp.push_back({count,i});
         }
         
-        sort(vp.begin(), vp.end()); // sort first by count and then by index 
+        sort(vp.begin(),vp.end());
         
         vector<int> ans;
-        for(int i=0; i<k; ++i) ans.push_back(vp[i].second);  // pick the first kth as they are the weakest after sorting 
+        
+        for(int i = 0; i < k; i++) {
+            ans.push_back(vp[i].second);
+        }
+
         return ans;
+        
     }
 };
