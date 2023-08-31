9d2a74b1ecb3c936ecb8ebeb428cf18292247004
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-08-31 16:48:32 -0700

    Time: 3 ms (39.12%), Space: 6.6 MB (24.17%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        //USES DFS
+        vector<string> phone {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        if(digits.empty()) {
+            return {};
+        }
+        
+        queue<string> q;
+        vector<string> ans;
+        
+        q.push(""); //intially empty string pushed in
+        
+        while(!q.empty()) {
+            string curr = q.front();
+            q.pop();
+            
+            if(curr.length() == digits.length()) { //checks if combination is equal length to digits, if it is, that means combination has been formed
+                ans.push_back(curr);
+            }
+            else {
+                string s = phone[digits[curr.length()]- '0']; //This line retrieves the string of letters corresponding to the next digit to be added to the combination. It uses the length of the current combination to find the digit index from the digits string. Then, it uses that digit index to access the corresponding string of letters from the phone vector.
+                
+                for(auto x: s) {
+                    q.push(curr + x); 
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
