4ea616ae3511abe27df46dbed990fefa89c4cd70
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-09-03 17:20:23 -0700

    Time: 3 ms (98.10%), Space: 18.3 MB (61.30%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 1;
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] != nums[i-1]) { //unique number found
-                nums[ans] = nums[i];
+                nums[ans] = nums[i]; //use ans index, ans only ever increments when nums[i] != nums[i-1] meaning we can basically use it to completely erase duplicates
                 ans++;
             }
         }
