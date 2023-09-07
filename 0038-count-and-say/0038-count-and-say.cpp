915c8035085ed148e234c0a8aee55de7c39c7ecd
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-09-06 18:53:05 -0700

    Time: 7 ms (68.79%), Space: 6.8 MB (51.69%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -4,16 +4,17 @@ public:
         if(n == 1) {
             return "1"; //base case
         }
-        
+        //starts at n = 4, and goes all the way down to base case, where n=1 which returns 1, and string s is evaluated to 1, so then we get one 1, (returned as "11"), meaning the next s = 11, which has size 2, etc. 
+        //this slowly builds up the final string we need
         string s = countAndSay(n-1);
         string ans = "";
         for(int i = 0; i < s.size(); i++) {
             int count = 1;
             while(i < (s.size()-1) && (s[i] == s[i+1])) {
                 i++;
-                count++;
+                count++; //counts how many consecutive characters are the same
             }
-            ans.append(to_string(count) + s[i]); //append is like push for strings
+            ans.append(to_string(count) + s[i]); //append is like push for strings, ex: three consectutive 2's, is 32 
         }
         return ans;
     }
