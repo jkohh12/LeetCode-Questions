f161ada81aef66d80d8287cdb5a39daa5fcccf52
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-09-08 16:51:50 -0700

    Time: 14 ms (6.26%), Space: 10.5 MB (5.23%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -4,13 +4,13 @@ public:
         if(nums.size() <= 1) {
             return {nums};
         }
-        vector<vector<int>> result;
+        vector<vector<int>> result; //store permutations
         for(int i = 0; i < nums.size(); i++) {
             vector<int> v(nums.begin(), nums.end()); //copying current nums vector
             v.erase(v.begin() + i); //erasing element at i of vector v
-            auto res = permute(v);
-            
-            for(int j = 0; j < res.size(); j++) {
+            auto res = permute(v); // recursively compute permutations of remaining elements, after ith element is removed
+            //returns vector<vector<int>> type
+            for(int j = 0; j < res.size(); j++) { //for each element in res, insert element at beginning and add to result vector
                 vector<int> _v = res[j];
                 _v.insert(_v.begin(), nums[i]);
                 result.push_back(_v);
