7f290946a136fc5b89a69396e1c590b1ac38e57e
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-09-15 16:49:03 -0700

    Time: 56 ms (42.15%), Space: 48.8 MB (21.20%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        //get index size
+        //ex: 
+        // 2 3 1 1 4 (max jump length)
+        // 0 1 2 3 4 (index)
+        //
+        int minJump = 0;
+        for(int i = nums.size()-2; i >= 0; i--) {
+            minJump++;
+            if(nums[i] >= minJump) {
+                minJump = 0; //basically it checks at every iteration if the max jump length is at least bigger than or equal to the min jump length required, if it is, then minJump is set to 0, redoing that cycle until it gets to the beginning of the vector
+                //example using array above, we start at nums[3], since nums.size()-2 = 3
+                //we increment minJump by 1, then check if nums[3] >= minJump, which in this case is, since 1 = 1
+                //next element we check is nums[2], same case as above, so its fine
+                //next element is nums[1], here the max jump length is 3 which is greater than minJump which at this point is 1 since it kept getting reset
+                //final element is nums[0], same as above so its fine
+            }
+        }
+        if(minJump == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
