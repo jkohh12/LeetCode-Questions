4e72444100393428bcc8fd7a58e6650a717402e6
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-09-15 17:00:08 -0700

    Time: 42 ms (87.06%), Space: 48.8 MB (18.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -25,4 +25,13 @@ public:
             return false;
         }
     }
+    //other solution
+    // bool canJump(vector<int>& nums) {
+    // int n = nums.size();
+    // int maxReach = 0;
+    // for (int i = 0; i < n; i++) {
+    //     if (i > maxReach) return false;
+    //     maxReach = max(maxReach, i + nums[i]);
+    // }
+    // return true;
 };
