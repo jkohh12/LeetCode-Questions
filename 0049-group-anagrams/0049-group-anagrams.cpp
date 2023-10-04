19135f19695591bc28c5502f35f680fd8749b5d7
Jin Koh <136869443+jkohh12@users.noreply.github.com>
2023-10-03 19:07:20 -0700

    Time: 26 ms (81.82%), Space: 21.1 MB (37.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -8,16 +8,16 @@ public:
         vector <vector<string>> ans;
         unordered_map <string, vector<string>> mp;
         for(int i = 0; i < strs.size(); i++) {
-            string str = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            mp[strs[i]].push_back(str);
+            string str = strs[i]; //holds onto string value before it is sorted
+            sort(strs[i].begin(), strs[i].end()); //sorts the string abc order
+            mp[strs[i]].push_back(str); //at the index strs[i], which is now sorted, push back the oirignal string value before it was sorted
             
         }
         
-        for(auto i : mp) {
+        for(auto i : mp) { //iterate through every element of mp with i
             ans.push_back(i.second);
         }
         return ans;
-
+        
     }
 };
